d75a94375854b3d559e16e1c2eeea68e59e68f79
Artem Starodubtsev <ayostar@gmail.com>
2022-01-15 14:30:40 +0300

    Third commit

--- dirty_main.py
+++ dirty_main.py
@@ -1,2 +1,6 @@
-from main import *
-main()
+from application.salary import *
+from application.db.people import *
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import application.salary as salary
 import application.db.people as people
 
 
-def print_hi(name):
+def print_hi():
+    user = input(f'Как к вам обращаться?: ')
     local_date = date.today()
-    print(f'Hi, {name}. Local data is: {local_date.strftime("%d-%m-%Y")}')
+    print(f'Hi, {user}. Local data is: {local_date.strftime("%d-%m-%Y")}')
 
 
 if __name__ == '__main__':
-    user = input(f'Как к вам обращаться?: ')
-    print_hi(user)
+    print_hi()
     salary.calculate_salary()
     people.get_employees()
